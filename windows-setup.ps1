c7be931eb08391c85f3c819e16ce471fe819c6df
debutinfotechpvtltd <37358796+debutinfotechpvtltd@users.noreply.github.com>
2023-05-30 12:47:06 +0530

    Update windows-setup.ps1

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -5,14 +5,33 @@ docker-compose up -d --build pega-postgres
 docker-compose up -d --build pega-installer
 
 # Wait for the installer container to finish installation and print "build success" message
-$containerLogs = docker-compose logs -f pega-installer
-while (-not ($containerLogs.Contains("BUILD SUCCESSFUL"))) {
-    $containerLogs = docker-compose logs -f pega-installer
-    Start-Sleep -Seconds 1
+$buildSuccess = $false
+
+# Wait for the installer container to finish installation and print "BUILD SUCCESSFUL" message
+while (-not $buildSuccess) {
+    $containerLogs = docker-compose logs pega-installer --tail=10  # Fetch last 10 lines of logs
+
+    # Print the logs to the console
+    Write-Host $containerLogs
+
+    if ($containerLogs -match "BUILD SUCCESSFUL") {
+        $buildSuccess = $true
+        
+        }
+
+    if (-not $buildSuccess) {
+        Start-Sleep -Seconds 1
+    }
 }
 
-# Wait for the installer container to terminate
-docker wait pega-installer
+# Wait for the installer container to exit
+$exitCode = docker wait pega-installer
 
-# Continue with other operations or starting the remaining containers(Start Web container)
-docker-compose up -d --build pega-web  
+# Continue with other operations or starting the remaining containers (Start Web container)
+
+if ($exitCode -eq 0) {
+    # Start the remaining containers (Start Web container)
+   docker-compose up -d --build pega-web
+} else {
+    Write-Host "Installer container failed with exit code: $exitCode"
+}
